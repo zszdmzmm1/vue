1af8207e393852b02103b957eabb1db59d3816e3
zszdmzmm <1075534380@qq.com>
2023-11-16 15:55:52 +0800

    step-5: Form Bindings (v-model)

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,9 @@
 import { ref } from 'vue'
 
 const text = ref('')
-
-function onInput(e) {
-  text.value = e.target.value
-}
 </script>
 
 <template>
-  <input :value="text" @input="onInput" placeholder="Type here">
+  <input v-model="text" placeholder="Type here">
   <p>{{ text }}</p>
 </template>
