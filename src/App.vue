d794b8d173b3ca76ed86f861577578a595b13fa5
zszdmzmm <1075534380@qq.com>
2023-11-16 15:47:54 +0800

    step-4: Event Listeners

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,12 @@
 import { ref } from 'vue'
 
 const count = ref(0)
+
+function increment() {
+  count.value++
+}
 </script>
 
 <template>
-  <!-- 使此按钮生效 -->
-  <button>count is: {{ count }}</button>
+  <button @click="increment">count is: {{ count }}</button>
 </template>
