31d04806d14a3a6cf844cb5457985489e5afb2da
zszdmzmm <1075534380@qq.com>
2023-11-16 16:00:12 +0800

    step-6: Conditional Rendering

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,12 @@ import { ref } from 'vue'
 const awesome = ref(true)
 
 function toggle() {
-  // ...
+  awesome.value = !awesome.value
 }
 </script>
 
 <template>
   <button @click="toggle">toggle</button>
-  <h1>Vue is awesome!</h1>
-  <h1>Oh no 😢</h1>
+  <h1 v-if="awesome">Vue is awesome!</h1>
+  <h1 v-else>Oh no 😢</h1>
 </template>
