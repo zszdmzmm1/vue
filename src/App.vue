0466dbbae9dcc0465bbf245fbf90a758d73af183
zszdmzmm <1075534380@qq.com>
2023-11-16 16:15:35 +0800

    step-8: Computed Property before

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,23 @@
 <script setup>
 import { ref } from 'vue'
 
-// 给每个 todo 对象一个唯一的 id
 let id = 0
 
 const newTodo = ref('')
+const hideCompleted = ref(false)
 const todos = ref([
-  { id: id++, text: 'Learn HTML' },
-  { id: id++, text: 'Learn JavaScript' },
-  { id: id++, text: 'Learn Vue' }
+  { id: id++, text: 'Learn HTML', done: true },
+  { id: id++, text: 'Learn JavaScript', done: true },
+  { id: id++, text: 'Learn Vue', done: false }
 ])
 
 function addTodo() {
-  todos.value.push(
-      {id: id++, text: newTodo.value}
-  )
+  todos.value.push({ id: id++, text: newTodo.value, done: false })
   newTodo.value = ''
 }
 
 function removeTodo(todo) {
-  todos.value = todos.value.filter(
-      (t) => t !== todo
-  )
+  todos.value = todos.value.filter((t) => t !== todo)
 }
 </script>
 
@@ -32,8 +28,18 @@ function removeTodo(todo) {
   </form>
   <ul>
     <li v-for="todo in todos" :key="todo.id">
-      {{ todo.text }}
+      <input type="checkbox" v-model="todo.done">
+      <span :class="{ done: todo.done }">{{ todo.text }}</span>
       <button @click="removeTodo(todo)">X</button>
     </li>
   </ul>
+  <button @click="hideCompleted = !hideCompleted">
+    {{ hideCompleted ? 'Show all' : 'Hide completed' }}
+  </button>
 </template>
+
+<style>
+.done {
+  text-decoration: line-through;
+}
+</style>
