48227b2d449364a6a7a463a9060b0e2ddc0613af
zszdmzmm <1075534380@qq.com>
2023-11-17 17:19:58 +0800

    AntIcon import one to one

--- src/AntDesign.vue
+++ src/AntDesign.vue
@@ -1,9 +1,19 @@
 <template>
-  <a-space wrap>
-    <a-button type="primary">Primary Button</a-button>
-    <a-button>Default Button</a-button>
-    <a-button type="dashed">Dashed Button</a-button>
-    <a-button type="text">Text Button</a-button>
-    <a-button type="link">Link Button</a-button>
+  <a-space>
+    <home-outlined/>
+    <setting-filled/>
+    <smile-outlined/>
+    <sync-outlined spin/>
+    <smile-outlined :rotate="180"/>
+    <loading-outlined/>
   </a-space>
 </template>
+<script  setup>
+import {
+  HomeOutlined,
+  SettingFilled,
+  SmileOutlined,
+  SyncOutlined,
+  LoadingOutlined,
+} from '@ant-design/icons-vue';
+</script>
