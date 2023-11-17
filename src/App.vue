c68405bace2ef190fb53c37896b5305922046708
zszdmzmm <1075534380@qq.com>
2023-11-17 16:21:21 +0800

    AntDesign a simple example

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,13 @@ import { ref, computed } from 'vue'
 import Home from './Home.vue'
 import SomeOtherPage from './SomeOtherPage.vue'
 import BookList from "./BookMap.vue";
+import AntDesign from './AntDesign.vue';
 
 const routes = {
   '/': Home,
   '/some-other-page': SomeOtherPage,
-  '/book-list': BookList
+  '/book-list': BookList,
+  '/ant-design': AntDesign
 }
 
 const currentPath = ref(window.location.hash)
@@ -26,6 +28,7 @@ const currentView = computed(() => {
     <li> <a href= "#/"> home page </a> </li>
     <li> <a href="#/some-other-page">SomeOtherPage</a> </li>
     <li> <a href="#/book-list">BookList</a> </li>
+    <li> <a href="#/ant-design">AntDesign</a> </li>
   </ul>
   <component :is="currentView" />
 </template>
